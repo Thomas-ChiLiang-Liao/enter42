f9260bc991c8a4539f3a6d41693bec78575d5eee
Thomas Liao <chilean.liao@gmail.com>
2024-03-07 11:06:48 +0800

    20240307校系資料查詢介面完成_資料表TVEREDepartment結構更改

--- departments/departmentDetails.php
+++ departments/departmentDetails.php
@@ -32,7 +32,7 @@ function dateFormat($date, $type) {
 }
 
 function memoContent($content) {
-  if (mb_substr($content,0,2,'UTF-8') == '1.') {
+   if (mb_substr($content,0,2,'UTF-8') == '1.') {
     $content = str_replace('\r','',$content);
     $rContent = '';
     $list = explode('。', $content);
@@ -54,7 +54,8 @@ $sql = "SELECT "
      . "  CONCAT(TVETExamSort.id, TVETExamSort.sort) AS examSort, "
      . "  TVEREDepartment.quotaA, TVEREDepartment.stage2QuotaA, TVEREDepartment.quotaB, TVEREDepartment.stage2QuotaB, "
      . "  TVEREDepartment.chineseMagnification, TVEREDepartment.englishMagnification, TVEREDepartment.mathMagnification, "
-     . "  TVEREDepartment.pro1Magnification, TVEREDepartment.pro2Magnification, TVEREDepartment.totalMagnification, "
+     . "  TVEREDepartment.pro1Magnification, TVEREDepartment.pro2Magnification, "
+     . "  TVEREDepartment.chineseFilterFlag, TVEREDepartment.englishFilterFlag, TVEREDepartment.mathFilterFlag, TVEREDepartment.pro1FilterFlag, TVEREDepartment.pro2FilterFlag, "
      . "  TVEREDepartment.chineseWeight, TVEREDepartment.englishWeight, TVEREDepartment.mathWeight, "
      . "  TVEREDepartment.pro1Weight, TVEREDepartment.pro2Weight, TVEREDepartment.examScoreRate, "
      . "  TVEREDepartment.assignItem1, TVEREDepartment.assignItem1Threshold, TVEREDepartment.assignItem1Rate, "
@@ -95,7 +96,7 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <title><?php echo $_GET['depid']; ?>簡章內容</title>
-  <link rel="icon" href="../images/logo.icon.png" type="image/x-icon">
+  <link rel="icon" href="../images/<?php echo ( $_SERVER["SERVER_NAME"] == "yy33.us" ? "website-design.png" : "logo.icon.png"); ?>" type="image/x-icon">
   <link rel="stylesheet" href="../styles.css">
 </head>
 <body>
@@ -103,15 +104,15 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
     <div class="row mt-2">
       <div class="col-12">
         <div class="alert alert-danger display-6 text-center align-middle">
-          <strong>警告！</strong>下表資料可能有誤，請以紙本簡章或<a href="https://www.jctv.ntut.edu.tw/downloads/112/apply/ugcdrom/index.html" target="_blank">官網</a>資料為主。
-          <a href="https://www.jctv.ntut.edu.tw/downloads/112/apply/ugcdrom/printDept.html?dCode=<?php echo $_GET['depid']; ?>" class="btn btn-secondary btn-lg">官網簡章列印</a>
+          <strong>警告！</strong>下表資料可能有誤，請以紙本簡章或<a href="https://www.jctv.ntut.edu.tw/downloads/113/apply/ugcdrom/index.html" target="_blank">官網</a>資料為主。
+          <a href="https://www.jctv.ntut.edu.tw/downloads/113/apply/ugcdrom/printDept.html?dCode=<?php echo $_GET['depid']; ?>" class="btn btn-secondary btn-lg">官網簡章列印</a>
         </div>
         <table class="table table-bordered table-sm">
           <tbody>
             <tr>
               <th class="table-warning text-center align-middle" rowspan="3">校系科組<br>學程名稱</th>
               <td class="table-light align-middle" rowspan="3" colspan="2"><?php echo "$result[schTitle]<br>$result[depTitle]"; ?></td>
-              <th class="table-warning text-center align-middle" colspan="3">第一階段</th>
+              <th class="table-warning text-center align-middle" colspan="4">第一階段</th>
               <th class="table-warning text-center align-middle" colspan="6">第二階段指定項目甄試</th>
               <th class="table-warning text-center align-middle" colspan="2">可選填報名之系<br>科(組)、學程數</th>
               <td class="bg-danger text-white text-center align-middle">
@@ -119,7 +120,7 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               </td>
             </tr>
             <tr>
-              <th class="table-warning text-center align-middle" colspan="3">統一入學測驗成績</th>
+              <th class="table-warning text-center align-middle" colspan="4">統一入學測驗成績</th>
               <th class="table-warning text-center align-middle" colspan="7">甄選總成績採計方式</th>
               <th class="table-warning text-center align-middle" colspan="2">總成績同分參酌方法</th>
             </tr>
@@ -127,6 +128,7 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <th class="table-warning text-center align-middle" rowspan="7">成<br>績<br>處<br>理<br>方<br>式</th>
               <th class="table-warning text-center align-middle">科目</th>
               <th class="table-warning text-center align-middle">篩選倍率</th>
+              <th class="table-warning text-center align-middle">同級分超額<br>篩選科目</th>
               <th class="table-warning text-center align-middle" colspan="2">統一入學測驗<br>成績加權</th>
               <th class="table-warning text-center align-middle">指定項目</th>
               <th class="table-warning text-center align-middle">最低得分</th>
@@ -143,8 +145,9 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <td class="table-light text-center align-middle">
                 <?php echo ( $result['chineseMagnification'] == null ? '--' : $result['chineseMagnification'] ); ?>
               </td>
+              <td class="table-light text-center align-middle"><?php echo $result['chineseFilterFlag']; ?></td>
               <td class="table-light text-center align-middle">
-                <?php echo ( $result['chineseWeight'] == null ? '--' : "✕$result[chineseWeight]0 倍" ); ?>
+                <?php echo ( $result['chineseWeight'] == null ? '--' : "✕$result[chineseWeight] 倍" ); ?>
               </td>
               <td class="table-light text-center align-middle" rowspan="6">合佔<br>總成績<br>比例<br><?php echo $result['examScoreRate']; ?>％</td>
               <td class="table-light text-center align-middle">
@@ -174,8 +177,9 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <td class="table-light text-center align-middle">
                 <?php echo ( $result['englishMagnification'] == null ? '--' : $result['englishMagnification'] ); ?>
               </td>
+              <td class="table-light text-center align-middle"><?php echo $result['englishFilterFlag']; ?></td>
               <td class="table-light text-center align-middle">
-                <?php echo ( $result['englishWeight'] == null ? '--' : "✕$result[englishWeight]0 倍" ); ?>
+                <?php echo ( $result['englishWeight'] == null ? '--' : "✕$result[englishWeight] 倍" ); ?>
               </td>
               <td class="table-light text-center align-middle">
                 <?php echo ( $result['assignItem2'] == null ? '--' : $result['assignItem2'] ); ?>
@@ -202,8 +206,9 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <td class="table-light text-center align-middle">
                 <?php echo ( $result['mathMagnification'] == null ? '--' : $result['mathMagnification'] ); ?>
               </td>
+              <td class="table-light text-center align-middle"><?php echo $result['mathFilterFlag']; ?></td>
               <td class="table-light text-center align-middle">
-                <?php echo ( $result['mathWeight'] == null ? '--' : "✕$result[mathWeight]0 倍" ); ?>
+                <?php echo ( $result['mathWeight'] == null ? '--' : "✕$result[mathWeight] 倍" ); ?>
               </td>
               <td class="table-light text-center align-middle">
                 <?php echo ( $result['assignItem3'] == null ? '--' : $result['assignItem3'] ); ?>
@@ -230,8 +235,9 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <td class="table-light text-center align-middle">
                 <?php echo ( $result['pro1Magnification'] == null ? '--' : $result['pro1Magnification'] ); ?>
               </td>
+              <td class="table-light text-center align-middle"><?php echo $result['pro1FilterFlag']; ?></td>
               <td class="table-light text-center align-middle">
-                <?php echo ( $result['pro1Weight'] == null ? '--' : "✕$result[pro1Weight]0 倍" ); ?>
+                <?php echo ( $result['pro1Weight'] == null ? '--' : "✕$result[pro1Weight] 倍" ); ?>
               </td>
               <td class="table-light text-center align-middle">
                 <?php echo ( $result['assignItem4'] == null ? '--' : $result['assignItem4'] ); ?>
@@ -255,8 +261,9 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <td class="table-light text-center align-middle">
                 <?php echo ( $result['pro2Magnification'] == null ? '--' : $result['pro2Magnification'] ); ?>
               </td>
+              <td class="table-light text-center align-middle"><?php echo $result['pro2FilterFlag']; ?></td>
               <td class="table-light text-center align-middle">
-                <?php echo ( $result['pro2Weight'] == null ? '--' : "✕$result[pro2Weight]0 倍" ); ?>
+                <?php echo ( $result['pro2Weight'] == null ? '--' : "✕$result[pro2Weight] 倍" ); ?>
               </td>
               <td class="table-light text-center align-middle">
                 <?php echo ( $result['assignItem5'] == null ? '--' : $result['assignItem5'] ); ?>
@@ -279,12 +286,12 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <th class="table-warning text-center align-middle">原住民考生</th>
               <td class="table-light text-center align-middle"><?php echo $result['quotaB']; ?></td>
               <td class="table-light text-center align-middle"><?php echo $result['stage2QuotaB']; ?></td>
-              <td class="table-light text-center align-middle">總級分</td>
-              <td class="table-light text-center align-middle">
-                <?php echo ( $result['totalMagnification'] == null ? '--' : "$result[totalMagnification]0" ); ?>
-              </td>
+              <td class="table-light text-center align-middle" colspan="3">同級分超額篩選為同級分超<br>額篩選勾選科目級分之總和</td>
               <td class="table-light text-center align-middle">--</td>
-              <td class="table-light text-center align-middle" colspan="4">--</td>
+              <td class="table-light text-center align-middle">--</td>
+              <td class="table-light text-center align-middle">--</td>
+              <td class="table-light text-center align-middle">--</td>
+              <td class="table-light text-center align-middle">--</td>
               <th class="table-warning text-center align-middle">6</th>
               <td class="table-light text-center align-middle"><?php echo sequenceContent($result['sequence6']); ?></td>
             </tr>
@@ -292,26 +299,26 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <th class="table-warning text-center align-middle" rowspan="4">指定項目<br>甄試費</th>
               <td class="table-light text-center align-middle" colspan="2" rowspan="4"><?php echo $result['assignItemExamFee'] ?> 元</td>
               <th class="table-warning text-center align-middle" colspan="3" rowspan="8">學習歷程<br>備審資料</th>
-              <th class="table-warning text-center align-middle" colspan="8">項目</th>
+              <th class="table-warning text-center align-middle" colspan="9">項目</th>
               <th class="table-warning text-center align-middle">上傳檔案件數上限</th>
             </tr>
             <tr>
-              <th class="table-warning text-left align-middle" colspan="8">A.修課紀錄&nbsp;&nbsp;※應屆畢業生一律由就讀高中學校上傳；非應屆畢業生或同等學力者，一律自行上傳歷年成績單(PDF檔)</th>
+              <th class="table-warning text-left align-middle" colspan="9">A.修課紀錄&nbsp;&nbsp;※應屆畢業生一律由就讀學校上傳；110學年度以後畢業生，一律由學習歷程中央資料庫提供；其餘畢業生或同等學力者，一律自行上傳歷年成績單(PDF檔)</th>
               <td class="table-light text-center align-middle">1件</td>
             </tr>
             <tr>
               <th class="table-warning text-left align-middle" colspan="2" rowspan="2">B.課程學習成果</th>
-              <th class="table-warning text-left align-middle" colspan="6">B-1.專題實作及實習科目學習成果(含技能領域)(*須至少上傳1件)</th>
+              <th class="table-warning text-left align-middle" colspan="7">B-1.專題實作、實習科目學習成果(含技能領域)(*須至少上傳1件)在符合上傳件數限下，可上傳專題實作、亦可上傳實習科目學習成果(含技能領域)、也可二者皆上傳</th>
               <td class="table-light text-center align-middle"><?php echo $result['B1']; ?>件</td>
             </tr>
             <tr>
-              <th class="table-warning text-left align-middle" colspan="6">B-2.其他課程學習(作品)成果</th>
+              <th class="table-warning text-left align-middle" colspan="7">B-2.其他課程學習(作品)成果</th>
               <td class="table-light text-center align-middle"><?php echo $result['B2']; ?>件</td>
             </tr>
             <tr>
               <th class="table-warning text-center align-middle" rowspan="3">學習歷程<br>備審資料<br>上傳暨繳費<br>截止時間</th>
               <td class="table-light text-center align-middle" colspan="2" rowspan="3"><?php echo dateFormat($result['date1'],0); ?>止</td>
-              <th class="table-warning text-left align-middle" colspan="8">
+              <th class="table-warning text-left align-middle" colspan="9">
                 C.多元表現：
                 <?php
                 $cString = '';
@@ -322,22 +329,22 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <td class="table-light text-center align-middle"><?php echo $result['C_counts']; ?>件</td>
             </tr>
             <tr>
-              <th class="table-warning text-left align-middle" colspan="8">D-1.多元表現綜整心得</th>
+              <th class="table-warning text-left align-middle" colspan="9">D-1.多元表現綜整心得</th>
               <td class="table-light text-center align-middle">1件</td>
             </tr>
             <tr>
-              <th class="table-warning text-left align-middle" colspan="8">D-2.學習歷程自述(含學習歷程反思、就讀動機、未來學習計畫與生涯規劃)</th>
+              <th class="table-warning text-left align-middle" colspan="9">D-2.學習歷程自述(含學習歷程反思、就讀動機、未來學習計畫與生涯規劃)</th>
               <td class="table-light text-center align-middle">1件</td>              
             </tr>
             <tr>
-              <th class="table-warning text-center align-middle" rowspan="2">公告第二階段<br>甄試名單並寄發<br>複試通知日期</th>
+              <th class="table-warning text-center align-middle" rowspan="2">公告第二階段<br>甄試名單<br>及注意事項</th>
               <td class="table-light text-center align-middle" colspan="2" rowspan="2"><?php echo ( $result['date2'] == null ? '--' : dateFormat($result['date2'],0).'起' ); ?></td>
-              <th class="table-warning text-left align-middle" colspan="8">D-3.其他有利審查資料</th>
+              <th class="table-warning text-left align-middle" colspan="9">D-3.其他有利審查資料</th>
               <td class="table-light text-center align-middle">1件</td>
             </tr>
             <tr>
               <th class="table-warning text-center align-middle" colspan="3" rowspan="3">學習歷程<br>備審資料<br>上傳說明</th>
-              <td class="table-light text-left align-middle" colspan="9" rowspan="3"><?php echo memoContent($result['uploadMemo']); ?></td>
+              <td class="table-light text-left align-middle" colspan="10" rowspan="3"><pre><?php echo $result['uploadMemo']; ?></pre></td>
             </tr>
             <tr>
               <th class="table-warning text-center align-middle">甄試日期</th>
@@ -351,7 +358,7 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
               <th class="table-warning text-center align-middle">甄選總成績<br>複查截止日期</th>
               <td class="table-light text-center align-middle" colspan="2"><?php echo dateFormat($result['date4'],0); ?>止</td>
               <th class="table-warning text-center align-middle" colspan="3" rowspan="4">指定項目甄試說明</th> 
-              <td class="table-light text-left align-middle" colspan="9" rowspan="4"><?php echo memoContent($result['assignExamMemo']); ?></td>             
+              <td class="table-light text-left align-middle" colspan="10" rowspan="4"><pre><?php echo $result['assignExamMemo']; ?></pre></td>             
             </tr>  
             <tr>
               <th class="table-warning text-center align-middle">公告正(備)取生<br>名單日期</th>
@@ -367,7 +374,7 @@ $result = $statement->fetch(PDO::FETCH_ASSOC);
             </tr>     
             <tr>
               <th class="table-warning text-center align-middle" colspan="3">備註</th>
-              <td class="table-light text-left align-middle" colspan="12"><?php echo memoContent($result['memo']); ?></td>
+              <td class="table-light text-left align-middle" colspan="14"><pre><?php echo $result['memo']; ?></pre></td>
             </tr>                         
           </tbody>
         </table>
