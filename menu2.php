3e9abe45dbee9dcb59cf8abb3111b501ce94ed6c
Thomas Liao <chilean.liao@gmail.com>
2024-05-06 20:58:07 +0800

    20240506查詢功能(表)修正

---
+++ menu2.php
@@ -0,0 +1,15 @@
+<?php
+function menu2() { 
+  GLOBAL $vhSchool;
+	session_start(); 
+?>
+
+	<div class="container-fluid">
+		<!-- 標題列 -->
+		<div class="row d-none d-sm-block">
+			<div class="col-12 m-0 pt-3 pb-1 bg-primary">
+				<h3 class="text-center text-white"><?php echo $vhSchool; ?>預選系統</h4>
+			</div>
+		</div>
+	</div>
+<?php } ?>
